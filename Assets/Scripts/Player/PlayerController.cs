97be45812b94a5cda80333a832ca9b85b7c942c2
Lopsitey <jakeastles13@gmail.com>
2025-03-12 12:03:00 +0000

    Added jumping and improved tiles
    Fixed the collisions on the tiles by clicking in the top left of the sprite editor and changing the custom physics collisions. This allowed me to make some of the colliders for the tiles diagonal, etcetera. The jump mechanic, could be polished slightly further for the jetpack and the tiles could be made better by tweaking the applied physics material.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,10 +2,13 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
+using Unity.Android.Gradle.Manifest;
 
 public class PlayerController : MonoBehaviour
 {
-    #region Framework Variables
+    #region Movement Variables
+    //Default to 0.0f, -1.0f which is facing down so if the player tries to fire a bullet without moving it will always have a valid direction
+    private Vector2 m_LastDirection = new Vector2(0.0f, -1.00f);
 
     //The inputs that we need to retrieve from the input system.
     private InputAction m_moveAction;
@@ -16,6 +19,14 @@ public class PlayerController : MonoBehaviour
     //The direction that the player is moving in.
     private Vector2 m_playerDirection;
 
+    //The amount of jumps the player has performed
+    private float m_jumpCounter;
+
+    //If the player is currently jumping
+    private bool m_isJumping;
+
+    private Vector2 m_vecGravity;
+
     [Header("Movement Parameters")]
     //The speed at which the player moves
     [SerializeField] private float m_playerSpeed = 200f;
@@ -23,19 +34,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float m_playerMaxSpeed = 1000f;
     //The maximum speed the player can move
     [SerializeField] private float m_playerMinSpeed = 50f;
-    [SerializeField] private float m_jumpHeight = 1.0f;
+    [SerializeField] private float m_jumpHeight = 25.0f;
+    [SerializeField] public float m_jumpTime = 0.4f;//essentially fuel
+    //For the jetpack
+    [SerializeField] public float m_jumpMultiplier = 2.0f;
 
     #endregion
 
-    //Default to 0.0f, -1.0f which is facing down so if the player tries to fire a bullet without moving it will always have a valid direction
-    private Vector2 m_LastDirection = new Vector2(0.0f, -1.00f);
     private int m_currentWeight = 0;
     private const int m_maxWeight = 0;
+
+    public Transform m_player;
+    public LayerMask m_platform;
 
     private void Awake()
     {
         m_moveAction = InputSystem.actions.FindAction("Move");
         m_rigidBody = GetComponent<Rigidbody2D>();
+        m_vecGravity = new Vector2(0, -Physics2D.gravity.y);
     }
 
     /// <summary>
@@ -55,7 +71,7 @@ public class PlayerController : MonoBehaviour
         //Clamp the speed to the maximum speed for if the speed has been changed in code
         float speed = Mathf.Clamp(m_playerSpeed, m_playerMinSpeed, m_playerMaxSpeed);
         //apply the movement to the character using the clamped speed value.
-        m_rigidBody.linearVelocity = m_playerDirection * (speed * Time.fixedDeltaTime);
+        m_rigidBody.linearVelocity = new Vector2(m_playerDirection.x * (speed * Time.fixedDeltaTime), m_rigidBody.linearVelocityY);// m_playerDirection * (speed * Time.fixedDeltaTime);
     }
 
     /// <summary>
@@ -74,6 +90,15 @@ public class PlayerController : MonoBehaviour
             {
                 m_LastDirection = m_playerDirection;//Sets the last facing direction of the player
             }
+        }
+
+        if (m_rigidBody.linearVelocityY > 0 && m_isJumping && m_hasFuel())
+        {
+            Debug.Log("Jumped!");
+            m_jumpCounter += Time.deltaTime;
+            if (m_jumpCounter > m_jumpTime)
+                m_isJumping = false;//stop jumping if you've been jumping for too long
+            m_rigidBody.linearVelocity += m_vecGravity * m_jumpMultiplier * Time.deltaTime;
         }
     }
 
@@ -95,10 +120,32 @@ public class PlayerController : MonoBehaviour
 
     public void HandleJump(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed)
+        if (ctx.performed && onGround())//jump started
         {
-            Debug.Log("Jumping");
-            m_rigidBody.AddForce(Vector2.up * m_jumpHeight, ForceMode2D.Impulse);
+            m_isJumping = true;
+            m_rigidBody.linearVelocity = new Vector2(m_rigidBody.linearVelocityX, m_jumpHeight);//m_rigidBody.AddForce(Vector2.up * m_jumpHeight, ForceMode2D.Force);
         }
+        if (ctx.canceled)//jump ended
+        {
+            m_isJumping= false;
+            m_jumpCounter = 0;
+            if (m_rigidBody.linearVelocityY > 0) 
+            {
+                //reduces the velocity by 60% after the jump has been completed to prevent further upward motion
+                m_rigidBody.linearVelocity = new Vector2(m_rigidBody.linearVelocityX, m_rigidBody.linearVelocityY * 0.6f);
+            }
+        }
+    }
+
+    bool onGround() 
+    {
+        return Physics2D.OverlapBox(m_player.position, new Vector2(1.0f, 1.0f), 0, m_platform);
+        //return Physics2D.Raycast(transform.position, -Vector2.up, 1f);
+    }
+
+    //If the player is currently jumping
+    bool m_hasFuel()
+    {
+        return true;//need to change this to check if jump time is greater than 0
     }
 }
